23a6ad64e17d64f608ed19015d25b42a28f3d9bd
Ashwin <ashwinparande1156610c@gmail.com>
2022-03-29 22:17:55 +0530

    Sidebar Added

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -10,13 +10,17 @@ const Navbar: React.FC = ({ children }) => {
       <div className=" flex items-center justify-start">{children}</div>
       <div className=" flex items-center text-xl justify-between gap-x-2 ">
         <button
-          className=" font-bold bg-indigo-700 hover:bg-indigo-600 dark:bg-orange-200 text-white dark:text-gray-900 dark:hover:bg-orange-300 p-2.5 md:p-3 rounded-md md:rounded-lg font-mono"
+          className=" font-bold bg-indigo-700 hover:bg-indigo-600 dark:bg-orange-200 text-white dark:text-black dark:hover:bg-orange-300 p-2.5 md:p-3 rounded-md md:rounded-lg font-mono"
           onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         >
           {theme === "light" ? <BiMoon /> : <BiSun />}
         </button>
-        <button className=" visible  md:hidden font-extrabold rounded-lg text-xl text-gray-900 dark:text-gray-100 p-3">
+        <button className=" group visible md:hidden font-extrabold rounded-lg text-xl text-gray-900 dark:text-gray-100 p-3">
           <AiOutlineMenu />
+          <div className="w-full -z-10 invisible group-focus:visible h-screen absolute top-0 left-0 bottom-0 right-0 bg-black/50" />
+          <div className="group text-base font-normal translate-x-[-100%] group-focus-within:translate-x-[0%] flex flex-col fixed top-0 left-0 bottom-0 bg-white transition-transform dark:bg-dark w-72 h-screen">
+            Hello
+          </div>
         </button>
       </div>
     </nav>
